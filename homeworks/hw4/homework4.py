3708a65d9ac8ebc09fd2f44e6bbb2c4e8b899be8
Chris Callison-Burch <ccb@upenn.edu>
2018-08-20 14:50:05 -0400

    HW4

---
+++ homeworks/hw4/homework4.py
@@ -0,0 +1,71 @@
+############################################################
+# CIS 521: Homework 4
+############################################################
+
+student_name = "Type your full name here."
+
+############################################################
+# Imports
+############################################################
+
+# Include your imports here, if any are used.
+
+
+
+############################################################
+# Section 1: Sudoku
+############################################################
+
+def sudoku_cells():
+    pass
+
+def sudoku_arcs():
+    pass
+
+def read_board(path):
+    pass
+
+class Sudoku(object):
+
+    CELLS = sudoku_cells()
+    ARCS = sudoku_arcs()
+
+    def __init__(self, board):
+        pass
+
+    def get_values(self, cell):
+        pass
+
+    def remove_inconsistent_values(self, cell1, cell2):
+        pass
+
+    def infer_ac3(self):
+        pass
+
+    def infer_improved(self):
+        pass
+
+    def infer_with_guessing(self):
+        pass
+
+############################################################
+# Section 2: Feedback
+############################################################
+
+feedback_question_1 = """
+Type your response here.
+Your response may span multiple lines.
+Do not include these instructions in your response.
+"""
+
+feedback_question_2 = """
+Type your response here.
+Your response may span multiple lines.
+Do not include these instructions in your response.
+"""
+
+feedback_question_3 = """
+Type your response here.
+Your response may span multiple lines.
+Do not include these instructions in your response.
+"""
