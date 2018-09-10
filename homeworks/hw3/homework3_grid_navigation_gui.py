1d9e7da0bc939e75d3fadebd6b6b29255839d1d9
Jie Gao <jiegao@Jies-MacBook-Pro.local>
2018-09-10 19:05:19 -0400

    Fix python2 syntax errors: tkinter, queue, PriorityQueue, tuple parameters.

--- homeworks/hw3/homework3_grid_navigation_gui.py
+++ homeworks/hw3/homework3_grid_navigation_gui.py
@@ -1,14 +1,14 @@
 import sys
-import Tkinter
+import tkinter
 
 import homework3
 
-class Grid(Tkinter.Canvas):
+class Grid(tkinter.Canvas):
 
     def __init__(self, master, scene, start_and_goal):
         self.rows, self.cols = len(scene), len(scene[0])
         self.square_size = min(40, 500 / self.rows, 500 / self.cols)
-        Tkinter.Canvas.__init__(self, master,
+        tkinter.Canvas.__init__(self, master,
             height=self.square_size * self.rows + 1,
             width=self.square_size * self.cols + 1, background="white")
         self.draw_scene(scene)
@@ -28,14 +28,14 @@ class Grid(Tkinter.Canvas):
                 self.create_rectangle(x0, y0, x1, y1, width=1, outline="black",
                     fill="gray50" if scene[row][col] else "white")
 
-    def transform(self, (row, col)):
+    def transform(self, row, col):
         x = self.square_size * (col + 0.5)
         y = self.square_size * (row + 0.5)
         return (x, y)
 
     def inverse_transform(self, event):
-        row = event.y / self.square_size
-        col = event.x / self.square_size
+        row = int(event.y / self.square_size)
+        col = int(event.x / self.square_size)
         return (row, col)
 
     def left_click(self, event):
@@ -61,50 +61,50 @@ class Grid(Tkinter.Canvas):
                     self.start_and_goal[0] = None
 
     def draw_point(self, point, color="black", tags=""):
-        x, y = self.transform(point)
+        x, y = self.transform(point[0], point[1])
         radius = self.square_size / 4.0
         return self.create_oval(x - radius, y - radius, x + radius, y + radius,
             fill=color, tags=tags)
 
     def draw_line(self, p, q, color="black", width=1, arrow=None, tags=""):
-        p_x, p_y = self.transform(p)
-        q_x, q_y = self.transform(q)
+        p_x, p_y = self.transform(p[0], p[1])
+        q_x, q_y = self.transform(q[0], q[1])
         return self.create_line(p_x, p_y, q_x, q_y, fill=color, width=width,
             arrow=arrow, tags=tags)
 
     def draw_path(self, path):
         for p, q in zip(path, path[1:]):
-            self.draw_line(p, q, color="blue", width=2, arrow=Tkinter.LAST,
+            self.draw_line(p, q, color="blue", width=2, arrow=tkinter.LAST,
                 tags="path")
 
     def clear_paths(self):
         self.delete("path")
 
-class GridNavigationGUI(Tkinter.Frame):
+class GridNavigationGUI(tkinter.Frame):
 
     def __init__(self, master, scene):
 
-        Tkinter.Frame.__init__(self, master)
+        tkinter.Frame.__init__(self, master)
 
         self.scene = scene
         self.start_and_goal = [None, None]
 
         self.grid = Grid(self, scene, self.start_and_goal)
-        self.grid.pack(side=Tkinter.LEFT, padx=1, pady=1)
+        self.grid.pack(side=tkinter.LEFT, padx=1, pady=1)
 
-        menu = Tkinter.Frame(self)
+        menu = tkinter.Frame(self)
 
-        Tkinter.Label(menu, text="Left click to specify the start point.").pack(
-            padx=1, pady=1, anchor=Tkinter.W)
-        Tkinter.Label(menu, text="Right click to specify the goal point.").pack(
-            padx=1, pady=1, anchor=Tkinter.W)
+        tkinter.Label(menu, text="Left click to specify the start point.").pack(
+            padx=1, pady=1, anchor=tkinter.W)
+        tkinter.Label(menu, text="Right click to specify the goal point.").pack(
+            padx=1, pady=1, anchor=tkinter.W)
 
-        Tkinter.Button(menu, text="Find Path", command=self.find_path_click).pack(
-            fill=Tkinter.X, padx=1, pady=1)
-        Tkinter.Button(menu, text="Clear Paths", command=self.clear_paths_click).pack(
-            fill=Tkinter.X, padx=1, pady=1)
+        tkinter.Button(menu, text="Find Path", command=self.find_path_click).pack(
+            fill=tkinter.X, padx=1, pady=1)
+        tkinter.Button(menu, text="Clear Paths", command=self.clear_paths_click).pack(
+            fill=tkinter.X, padx=1, pady=1)
         
-        menu.pack(side=Tkinter.RIGHT)
+        menu.pack(side=tkinter.RIGHT)
 
     def find_path_click(self):
         start, goal = self.start_and_goal
@@ -131,18 +131,18 @@ def load_scene(scene_path):
                         (char, row, col))
                     return None
     if len(scene) < 1:
-        print "Scene must have at least one row"
+        print("Scene must have at least one row")
         return None
     if len(scene[0]) < 1:
-        print "Scene must have at least one column"
+        print("Scene must have at least one column")
         return None
     if not all(len(row) == len(scene[0]) for row in scene):
-        print "Not all rows are of equal length"
+        print("Not all rows are of equal length")
         return None
     return scene
 
 if __name__ == "__main__":
-    root = Tkinter.Tk()
+    root = tkinter.Tk()
     root.title("Grid Navigation")
     scene = load_scene(sys.argv[1])
     if scene is not None:
