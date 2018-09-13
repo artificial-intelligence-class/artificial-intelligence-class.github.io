525e2fdc55fc1ce736453b01cb8da84c710f764b
Jie Gao <jiegao@Jies-MacBook-Pro.local>
2018-09-12 20:17:38 -0400

    Remove the game part from the skeleton file.

--- homeworks/hw3/homework3.py
+++ homeworks/hw3/homework3.py
@@ -66,51 +66,7 @@ def solve_distinct_disks(length, n):
     pass
 
 ############################################################
-# Section 4: Dominoes Game
-############################################################
-
-def create_dominoes_game(rows, cols):
-    pass
-
-class DominoesGame(object):
-
-    # Required
-    def __init__(self, board):
-        pass
-
-    def get_board(self):
-        pass
-
-    def reset(self):
-        pass
-
-    def is_legal_move(self, row, col, vertical):
-        pass
-
-    def legal_moves(self, vertical):
-        pass
-
-    def perform_move(self, row, col, vertical):
-        pass
-
-    def game_over(self, vertical):
-        pass
-
-    def copy(self):
-        pass
-
-    def successors(self, vertical):
-        pass
-
-    def get_random_move(self, vertical):
-        pass
-
-    # Required
-    def get_best_move(self, vertical, limit):
-        pass
-
-############################################################
-# Section 5: Feedback
+# Section 4: Feedback
 ############################################################
 
 feedback_question_1 = """
