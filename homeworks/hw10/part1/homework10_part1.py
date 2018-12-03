bcba332de456253e679599a281b940bd07d5c018
Jie Gao <jiegao@nichols-ve503-0943.apn.wlan.upenn.edu>
2018-12-03 18:29:28 -0500

    Split out hw10 extra credit part 1.

---
+++ homeworks/hw10/part1/homework10_part1.py
@@ -0,0 +1,59 @@
+############################################################
+# CIS 521: Homework 10 - Part 1
+############################################################
+
+student_name = "Type your full name here."
+
+############################################################
+# Imports
+############################################################
+
+# Include your imports here, if any are used.
+
+
+
+############################################################
+# Part 1: Individual Functions
+############################################################
+
+def convolve_greyscale(image, kernel):
+    pass
+
+
+def convolve_rgb(image, kernel):
+    pass
+
+
+def max_pooling(image, kernel, stride):
+    pass
+
+
+def average_pooling(image, kernel, stride):
+    pass
+
+
+def sigmoid(x):
+    pass
+
+
+############################################################
+# Feedback
+############################################################
+
+feedback_question_1 = """
+Type your response here.
+Your response may span multiple lines.
+Do not include these instructions in your response.
+"""
+
+feedback_question_2 = """
+Type your response here.
+Your response may span multiple lines.
+Do not include these instructions in your response.
+"""
+
+feedback_question_3 = """
+Type your response here.
+Your response may span multiple lines.
+Do not include these instructions in your response.
+"""
