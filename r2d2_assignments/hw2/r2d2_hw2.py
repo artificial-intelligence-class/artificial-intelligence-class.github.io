585fb2352e969556e798d6d44827d1e97d16d8a8
Yue Yang <38372825+YueYANG1996@users.noreply.github.com>
2019-10-22 18:29:40 -0400

    Update r2d2_hw2.py

--- r2d2_assignments/hw2/r2d2_hw2.py
+++ r2d2_assignments/hw2/r2d2_hw2.py
@@ -179,7 +179,43 @@ def printpath(G, start, goal, path):
 					else:
 						print_row += '--- '
 		print(print_row)
-
+def printtsp(G, start, goals, path):
+	rows = G.vertics[-1][0] + 1
+	cols = G.vertics[-1][1] + 1
+	for i in range(2 * rows - 1):
+		print_row = ''
+		if i % 2 == 0:
+			for j in range(cols):
+				current_node = (int(i / 2), j)
+				right_node = (int(i / 2), j + 1)
+				if current_node in goals:
+					pattern = '☒'
+				elif current_node in path:
+					pattern = '☑'
+				else:
+					pattern = '☐'
+				if (current_node, right_node) in G.edges and (right_node, current_node) in G.edges:
+					print_row += pattern + ' ' + '  '
+				else:
+					if right_node in G.vertics:
+						print_row += pattern + ' ' + '| '
+					else:
+						print_row += pattern + ' ' + '  '
+		else:
+			for j in range(cols):
+				current_node = (math.ceil(i/2), j)
+				up_node = (math.ceil(i/2) - 1, j)
+				if j == 0:
+					if (current_node, up_node) in G.edges and (up_node, current_node) in G.edges: 
+						print_row += '  ' + ' '
+					else:
+						print_row += '-- '
+				else: 
+					if (current_node, up_node) in G.edges and (up_node, current_node) in G.edges:
+						print_row += '  ' + '  '
+					else:
+						print_row += '--- '
+		print(print_row)
 ###########For GUI, Do not change#############
 def parse_scene(scene_file):
     if not os.path.exists(scene_file):
